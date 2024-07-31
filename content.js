2dfb9c31e1e7a59bd0c25b0fb9fcab37a151d26a
vanad1um4 <Vanad1um3@gmail.com>
2024-08-01 00:38:14 +0400

    🔧 a little unshittification

--- content.js
+++ content.js
@@ -1,37 +1,46 @@
-if (typeof window.intervalId === 'undefined') {
-  window.intervalId = null;
-
-  function findAndClickSkipButton() {
-    const button = document.querySelector('.ytp-skip-ad-button');
-    if (button) {
-      button.click();
-    }
-  }
-
-  function startMonitoring() {
-    if (!window.intervalId) {
-      window.intervalId = setInterval(findAndClickSkipButton, 1000);
-    }
-  }
-
-  function stopMonitoring() {
-    if (window.intervalId) {
-      clearInterval(window.intervalId);
-      window.intervalId = null;
-    }
-  }
-
-  chrome.storage.local.get(['isMonitoring'], (result) => {
-    if (result.isMonitoring) {
-      startMonitoring();
-    }
-  });
-
-  chrome.runtime.onMessage.addListener((request) => {
-    if (request.action === 'start') {
-      startMonitoring();
-    } else if (request.action === 'stop') {
-      stopMonitoring();
-    }
-  });
+function findAndClickSkipButton() {
+  const button = document.querySelector('.ytp-skip-ad-button');
+  if (button) button.click();
 }
+
+function startMonitoring() {
+  if (!window.observer) {
+    const lastInvocationTime = { value: 0 };
+
+    const observer = new MutationObserver(() => {
+      const currentTime = Date.now();
+      if (currentTime - lastInvocationTime.value > 1000) {
+        findAndClickSkipButton();
+        lastInvocationTime.value = currentTime;
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+
+    window.observer = observer;
+  }
+}
+
+function stopMonitoring() {
+  if (window.observer) {
+    window.observer.disconnect();
+    window.observer = null;
+  }
+}
+
+chrome.storage.local.get(['isMonitoring'], (result) => {
+  if (result.isMonitoring) {
+    startMonitoring();
+  }
+});
+
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.action === 'start') {
+    startMonitoring();
+  } else if (request.action === 'stop') {
+    stopMonitoring();
+  }
+});
